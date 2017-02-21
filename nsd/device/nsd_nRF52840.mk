464385c2959d51f1c8215e47f7a9b0ec7062c7e2
Karol Lasończyk <karol@lasonczyk.pl>
2017-02-21 02:00:49 +0100

    overall: Changed startup file from assembly to c

--- nsd/device/nsd_nRF52840.mk
+++ nsd/device/nsd_nRF52840.mk
@@ -1,6 +1,7 @@
 # Source files common to nrf52840
 NSD_SRC_FILES += \
-  $(NSD_ROOT)/device/nRF52840/nsd_mnd_nrf52840.c
+  $(NSD_ROOT)/device/nRF52840/nsd_mnd_nrf52840.c \
+  $(NSD_ROOT)/device/nRF52840/startup_nrf52840.c
 
 
 # Include folders common to nrf52840
